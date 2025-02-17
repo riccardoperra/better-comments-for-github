0ad82780b868f6670549b238cf924c5c4296dac3
riccardoperra <riccardo.perra@icloud.com>
2025-02-17 21:44:19 +0100

    force text area to sync in react (please don't see this)

--- src/editor/utils/forceGithubTextAreaSync.ts
+++ src/editor/utils/forceGithubTextAreaSync.ts
@@ -1,3 +1,30 @@
 export function forceGithubTextAreaSync(textarea: HTMLTextAreaElement) {
-  textarea.dispatchEvent(new Event('change', { bubbles: true }))
+  const event = new Event('change', {
+    bubbles: true,
+    composed: true,
+  })
+  Object.assign(event, {
+    isDefaultPrevented: () => false,
+  })
+  Object.defineProperty(event, 'target', {
+    writable: false,
+    value: textarea,
+  })
+  for (const key in textarea) {
+    if (key.includes('__reactProps')) {
+      const fiberProps = textarea[key as keyof typeof textarea] as Record<
+        string,
+        any
+      >
+      if (
+        fiberProps &&
+        'onChange' in fiberProps &&
+        typeof fiberProps.onChange === 'function'
+      ) {
+        fiberProps.onChange(event)
+      }
+
+      break
+    }
+  }
 }
