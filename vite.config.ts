563528e8836fe8babe7dcd6a43de86fc837d0eb3
riccardoperra <riccardo.perra@icloud.com>
2025-02-19 22:54:23 +0100

    add vite plugin crx workaround work world MAIN scripts

--- vite.config.ts
+++ vite.config.ts
@@ -20,13 +20,7 @@ const generateManifest = () => {
 }
 
 export default defineConfig({
-  plugins: [
-    solidPlugin(),
-    crx({ manifest }),
-    // webExtension({
-    //   manifest: generateManifest,
-    // }),
-  ],
+  plugins: [solidPlugin(), crx({ manifest })],
   // build: {
   //   minify: 'esbuild' as const,
   // },
@@ -34,41 +28,39 @@ export default defineConfig({
   //   keepNames: true,
   //   minifyIdentifiers: false,
   // },
-  server: {
-    port: 3000,
+  build: {
+    minify: false,
+    target: 'esnext',
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'crx:dynamic-imports-polyfill',
+          generateBundle(_, bundle) {
+            console.log('Generate')
+            const polyfill = `
+                (function () {
+                  const chrome = window.chrome || {};
+                  chrome.runtime = chrome.runtime || {};
+                  chrome.runtime.getURL = chrome.runtime.getURL || function(path) { return path.replace("assets/", "./"); };
+                })();
+            `
+            for (const chunk of Object.values(bundle)) {
+              if (
+                chunk.name?.endsWith('-loader.js') &&
+                'source' in chunk &&
+                typeof chunk.source === 'string' &&
+                chunk.source.includes('chrome.runtime.getURL') &&
+                !chunk.source.includes(polyfill)
+              ) {
+                chunk.source = `
+                  ${polyfill}
+                  ${chunk.source}
+                `
+              }
+            }
+          },
+        },
+      ],
+    },
   },
-  // build: {
-  //   minify: false,
-  //   target: 'esnext',
-  //   rollupOptions: {
-  //     plugins: [
-  //       {
-  //         name: 'crx:dynamic-imports-polyfill',
-  //         generateBundle(_, bundle) {
-  //           const polyfill = `
-  //               (function () {
-  //                 const chrome = window.chrome || {};
-  //                 chrome.runtime = chrome.runtime || {};
-  //                 chrome.runtime.getURL = chrome.runtime.getURL || function(path) { return path.replace("assets/", "./"); };
-  //               })();
-  //           `
-  //           for (const chunk of Object.values(bundle)) {
-  //             if (
-  //               chunk.name?.endsWith('-loader.js') &&
-  //               'source' in chunk &&
-  //               typeof chunk.source === 'string' &&
-  //               chunk.source.includes('chrome.runtime.getURL') &&
-  //               !chunk.source.includes(polyfill)
-  //             ) {
-  //               chunk.source = `
-  //                 ${polyfill}
-  //                 ${chunk.source}
-  //               `
-  //             }
-  //           }
-  //         },
-  //       },
-  //     ],
-  //   },
-  // },
 })
