c5300010a174cd9ec25dce0513e8c9540e65d58f
riccardoperra <riccardo.perra@icloud.com>
2025-03-01 12:02:19 +0100

    wip implementation for code block with custom language selector

--- src/core/editor/code-block/code-block-view.tsx
+++ src/core/editor/code-block/code-block-view.tsx
@@ -15,15 +15,22 @@
  */
 
 import { shikiBundledLanguagesInfo } from 'prosekit/extensions/code-block'
-import { For } from 'solid-js'
+import { createMemo, createSignal, onCleanup } from 'solid-js'
+import { Search } from '@kobalte/core/search'
+import { clsx } from 'clsx'
+import { createControllableSignal } from '@kobalte/core'
+import { useNodeViewContext } from '@prosemirror-adapter/solid'
 import { NodeViewWrapper } from '../nodeviews/node-view'
+import { Popover, PopoverContent, PopoverTrigger } from '../popover/popover'
 import styles from './code-block-view.module.css'
 import type { NodeViewContextProps } from '@prosemirror-adapter/solid'
 import type { CodeBlockAttrs } from 'prosekit/extensions/code-block'
 
 export default function CodeBlockView(props: NodeViewContextProps) {
+  const context = useNodeViewContext()
+  const attrLanguage = createMemo(() => context().node.attrs.language)
   const language = () => {
-    const lang = props.node.attrs.language
+    const lang = attrLanguage()
     switch (lang) {
       case 'js':
         return 'javascript'
@@ -34,28 +41,111 @@ export default function CodeBlockView(props: NodeViewContextProps) {
     }
   }
 
-  const setLanguage = (language: string) => {
-    const attrs: CodeBlockAttrs = { language }
-    props.setAttrs(attrs)
+  const setLanguage = (language: string | null) => {
+    const attrs: CodeBlockAttrs = { language: language ?? '' }
+    context().setAttrs(attrs)
   }
 
   return (
     <NodeViewWrapper>
       <div class={`highlight ${styles.CodeBlock}`}>
         <div class={styles.LanguageSelector} contentEditable={false}>
-          <select
-            class={styles.Select}
-            onChange={(event) => setLanguage(event.target.value)}
-            value={language() || ''}
-          >
-            <option value="">Plain Text</option>
-            <For each={shikiBundledLanguagesInfo}>
-              {(info) => <option value={info.id}>{info.name}</option>}
-            </For>
-          </select>
+          <CodeBlockLanguageSelector
+            value={language()}
+            onValueChange={setLanguage}
+          />
         </div>
         <pre ref={props.contentRef} data-language={language}></pre>
       </div>
     </NodeViewWrapper>
+  )
+}
+
+export function CodeBlockLanguageSelector(props: {
+  value: string
+  onValueChange: (value: string | null) => void
+}) {
+  const [open, setOpen] = createSignal(false)
+  const [term, setTerm] = createSignal('')
+  const [value, setValue] = createControllableSignal<string | null>({
+    value: () => props.value,
+    onChange: (value) => {
+      props.onValueChange(value)
+    },
+    defaultValue: () => null,
+  })
+  const options = shikiBundledLanguagesInfo
+
+  const filteredOptions = createMemo(() => {
+    const termValue = term()
+    if (!termValue) {
+      return options
+    }
+    return options.filter((option) =>
+      option.name.toLowerCase().includes(term().toLowerCase()),
+    )
+  })
+
+  const onPopoverKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setOpen(false)
+    }
+  }
+
+  let languageListboxContainer!: HTMLDivElement
+
+  return (
+    <Popover open={open()} onOpenChange={setOpen}>
+      <div class={'FormControl-select-wrap'}>
+        <PopoverTrigger
+          class={`FormControl FormControl-select FormControl-small ${styles.languageTrigger}`}
+        >
+          {value() ?? 'No value'}
+        </PopoverTrigger>
+      </div>
+      <PopoverContent
+        ref={() => {
+          onCleanup(() => setTerm(''))
+        }}
+        class={styles.LanguageSelectorPopover}
+        onKeyDown={onPopoverKeyDown}
+      >
+        <Search
+          open
+          options={filteredOptions()}
+          onInputChange={setTerm}
+          optionValue="name"
+          optionLabel="name"
+          onChange={(value) => {
+            setValue(value ? value.name : null)
+            setOpen(false)
+          }}
+          shouldFocusWrap={true}
+          placeholder="Search a language…"
+          itemComponent={(props) => (
+            <Search.Item
+              item={props.item}
+              class={clsx(styles.languageMenuItem)}
+            >
+              <Search.ItemLabel>{props.item.rawValue.name}</Search.ItemLabel>
+            </Search.Item>
+          )}
+        >
+          <Search.Control aria-label="Emoji">
+            <Search.Input
+              placeholder={'Search'}
+              class={'FormControl FormControl-input FormControl-small'}
+            />
+          </Search.Control>
+          <div class={styles.LanguageListboxRef} ref={languageListboxContainer}>
+            <Search.Listbox
+              class={styles.languageMenu}
+              scrollRef={() => languageListboxContainer}
+            />
+            <Search.NoResult>No languages found</Search.NoResult>
+          </div>
+        </Search>
+      </PopoverContent>
+    </Popover>
   )
 }
