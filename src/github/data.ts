10ff8da4c5acfd46d201c229220065a1105fa373
riccardoperra <riccardo.perra@icloud.com>
2025-05-19 22:19:09 +0200

    fix emoji menu

--- src/github/data.ts
+++ src/github/data.ts
@@ -54,6 +54,17 @@ export function fetchIssues(url: string): Promise<{
   }).then((result) => result.json())
 }
 
+export function fetchEmojisImages(
+  url: string,
+): Promise<Record<string, string>> {
+  return fetch('https://api.github.com/emojis', {
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      Accept: 'application/json',
+    },
+  }).then((result) => result.json())
+}
+
 export async function tryGetReferences(
   url: string,
 ): Promise<SuggestionData['references']> {
