0e953a01e9e0025903910c573ee78623a66c4059
riccardoperra <riccardo.perra@icloud.com>
2025-06-08 18:11:25 +0200

    ci release changeset

---
+++ scripts/check-tag.mjs
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 Riccardo Perra
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import path from 'node:path'
+import * as fs from 'node:fs'
+import { execSync } from 'node:child_process'
+
+const dirname = import.meta.dirname
+
+const statusPath = path.posix.join(dirname, '../release-meta.json')
+const cwd = process.cwd()
+
+const mainExtensionVersion = path.join(dirname, '../extension/package.json')
+
+const { version } = JSON.parse(fs.readFileSync(mainExtensionVersion, 'utf8'))
+
+const tag = `v${version}`
+
+// Get list of tags and check for the target
+const tags = execSync('git tag', { encoding: 'utf-8' })
+  .split('\n')
+  .map((t) => t.trim())
+const exists = tags.includes(tag)
+
+// Set output in GitHub Actions
+const githubOutput = process.env.GITHUB_OUTPUT
+if (githubOutput) {
+  fs.appendFileSync(githubOutput, `tag_exists=${exists}\n`)
+}
